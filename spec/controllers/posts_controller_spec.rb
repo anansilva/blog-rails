df8a40c364f9abffc46c94e4d51e0abbddac61d6
Ana Silva <ananunesdasilva@gmail.com>
2019-09-21 18:38:33 +0100

    PostsController test refactoring

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -25,9 +25,7 @@ describe PostsController do
   end
 
   describe '#show' do
-    let(:post) do
-      create(:post, title: 'This is my selected post', body: 'this is my body')
-    end
+    let(:post) { create(:post) }
 
     context 'when requesting in html format' do
       before { get :show, params: { id: post.id } }
@@ -49,25 +47,29 @@ describe PostsController do
   describe '#new' do
     it 'responds successfully' do
       get :new
+
       expect(response.status).to eq(200)
     end
   end
 
   describe '#create' do
+    post_params = { title: 'hey', body: 'to sexy for my shirt' }
+
     it 'responds successfully' do
-      post :create, params: { post: { title: 'hey', body: 'to sexy for my shirt' } }
+      post :create, params: { post: post_params }
 
       expect(response.status).to eq(302)
     end
 
-    it 'creates a Post with a body' do
-      post :create, params: { post: { title: 'hey', body: 'to sexy for my shirt' } }
+    it 'creates a Post with an ActionText body' do
+      post :create, params: { post: post_params }
 
       expect(Post.last.title).to eq('hey')
       expect(Post.last.body.class).to eq(ActionText::RichText)
+      expect(Post.last.body.body.to_html).to eq('to sexy for my shirt')
     end
   end
-  
+
   describe '#edit' do
     let(:post) { create(:post) }
 
@@ -79,12 +81,24 @@ describe PostsController do
   end
 
   describe '#update' do
-    let(:post) { create(:post, title: 'current title', body: 'your body is a wonderland') }
+    let(:post) do
+      create(:post, title: 'current title', body: 'your body is a wonderland')
+    end
 
-    it 'updates the post' do
-      put :update, params: { id: post.id, post: { title: 'this is a new title'} }
+    it 'updates the post when updating title' do
+      new_title = 'this is a new title'
+
+      put :update, params: { id: post.id, post: { title: new_title } }
 
       expect(post.reload.title).to eq('this is a new title')
+    end
+
+    it 'updates the post when updating body' do
+      new_body = 'this is a new body'
+
+      put :update, params: { id: post.id, post: { body: new_body } }
+
+      expect(post.reload.body.body.to_html).to eq('this is a new body')
     end
   end
 
@@ -95,6 +109,12 @@ describe PostsController do
       delete :destroy, params: { id: post.id }
 
       expect { post.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'redirects to the index page' do
+      delete :destroy, params: { id: post.id }
+
+      expect(response.status).to eq(302)
     end
   end
 end
