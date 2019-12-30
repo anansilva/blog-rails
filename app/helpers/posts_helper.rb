3397eecee005a54af0639c1b47a226d5619e94d6
Ana Silva <ananunesdasilva@gmail.com>
2019-12-30 21:46:08 +0000

    Hover and active classes on tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+  def active_tag(name)
+    'text-gray-900' if params[:tag] == name
+  end
+
+  def hover_tag(name)
+    'hover:text-gray-700' unless params[:tag] == name
+  end
+
+  def all_tag
+    return 'text-gray-900' unless params[:tag].present?
+    'hover:text-gray-700'
+  end
 end
