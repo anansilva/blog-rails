25b983525f7256abde84410ed66bca6cf10ea6dc
Ana Silva <ananunesdasilva@gmail.com>
2020-01-06 20:24:05 +0000

    Add test context when login is not valid

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -12,7 +12,23 @@ describe SessionsController do
       end
 
       it 'redirects to the new post page' do
-        expect(response.status).to eq(302)
+        expect(response).to redirect_to(new_post_path)
+      end
+    end
+
+    context 'when the login data is invalid' do
+      before do
+        create(:user, email: 'anabanana@test.com', password: 'banana')
+
+        post :create, params: { email: 'user@test.com', password: '$p@ssw0rd'}
+      end
+
+      it 'does not add the user id to the rails session hash' do
+        expect(session[:user_id]).to eq(nil)
+      end
+
+      it 'redirects to the sessions page' do
+        expect(response).to redirect_to(sessions_path)
       end
     end
   end
