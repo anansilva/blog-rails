6df05096ace705cc09455e268bcde1d6e2345690
Ana Silva <ananunesdasilva@gmail.com>
2020-01-17 17:39:06 +0000

    Add logout button when there is a current_user

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
-  delete '/sessions', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 end
