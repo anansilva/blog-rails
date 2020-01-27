2bf1d49db671a9b766be2d19ac249568fd1272e1
Ana Silva <ananunesdasilva@gmail.com>
2020-01-27 21:10:16 +0000

    Fix meta tags rubocop offenses

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,13 +1,13 @@
 module MetaTagsHelper
   def meta_title(title)
-    content_for(:title, title) || 'Web development personal blog'
+    content_for(:title, title)
   end
 
   def meta_description(description)
-    content_for(:meta_description, description)  || 'Content on web development mainly focused on ruby and rails'
+    content_for(:meta_description, description)
   end
 
   def meta_keywords(keywords)
-    content_for(:meta_keywords, keywords) || 'web development, ruby, rails, tools, tdd, rspec'
+    content_for(:meta_keywords, keywords)
   end
 end
