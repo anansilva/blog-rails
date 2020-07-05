f39885cbea09024d1a8add4ccca530cd3cb3df81
Ana Silva <ananunesdasilva@gmail.com>
2020-07-05 17:09:56 +0100

    Implement callable module to use in services

---
+++ lib/services/callable.rb
@@ -0,0 +1,34 @@
+module Services
+  module Callable
+    module ClassMethods
+      def call(*args)
+        new(*args).result
+      end
+
+      def receive(*arguments)
+        define_method(:initialize) do |*parameters|
+          unless arguments.size == parameters.size
+            raise ArgumentError, 'wrong number of arguments' +
+              " given #{parameters.size}, expected #{arguments.size}"
+          end
+
+          arguments.zip(parameters).each do |argument, parameter|
+            if argument.to_s == 'params'
+              instance_variable_set("@params", parameter || {})
+            else
+              instance_variable_set("@#{argument}", parameter)
+            end
+          end
+        end
+      end
+    end
+
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    def result
+      fail NotImplementedError
+    end
+  end
+end
