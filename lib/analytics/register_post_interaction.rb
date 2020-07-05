302587a2504036714ee30c973f70889776e28116
Ana Silva <ananunesdasilva@gmail.com>
2020-07-05 15:07:04 +0100

    Absctract registration of post interaction to a service

---
+++ lib/analytics/register_post_interaction.rb
@@ -0,0 +1,41 @@
+module Analytics
+  class RegisterPostInteraction
+    ALLOWED_COUNTERS = {
+      post_viewed: 'views_count'
+    }.freeze
+
+    def initialize(event)
+      @event = event
+    end
+
+    def self.call(event)
+      new(event).result
+    end
+
+    def result
+      find_or_initialize_counter.tap do |visitor_post_daily_counter|
+        visitor_post_daily_counter
+          .update(views_count: visitor_post_daily_counter.send(interaction_counter) + 1)
+      end
+    end
+
+    private
+
+    def interaction_counter
+      ALLOWED_COUNTERS[event_type.to_sym]
+    end
+
+    def event_type
+      @event.type.split('::').last.underscore
+    end
+
+    def find_or_initialize_counter
+      ::Analytics::VisitorPostDailyCounter
+        .find_or_initialize_by(
+          post_id: @event.data[:post_id],
+          day: Date.today,
+          visitor_ip: @event.metadata[:request_ip]
+      )
+    end
+  end
+end
--- lib/event_sourcing/subscribers/post_interactions.rb
+++ lib/event_sourcing/subscribers/post_interactions.rb
@@ -4,21 +4,7 @@ module EventSourcing
       def call(event)
         ::Analytics::RegisterDailyVisit.call(event)
 
-        find_or_initialize_counter(event).tap do |visitor_post_daily_counter|
-          visitor_post_daily_counter
-            .update(views_count: visitor_post_daily_counter.views_count + 1)
-        end
-      end
-
-      private
-
-      def find_or_initialize_counter(event)
-        ::Analytics::VisitorPostDailyCounter
-          .find_or_initialize_by(
-            post_id: event.data[:post_id],
-            day: Date.today,
-            visitor_ip: event.metadata[:request_ip]
-          )
+        ::Analytics::RegisterPostInteraction.call(event)
       end
     end
   end
