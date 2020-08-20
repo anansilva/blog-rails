80f1424caafcebb652896a9164e1bb97497a4abc
Ana Silva <ananunesdasilva@gmail.com>
2020-08-20 22:16:11 +0100

    add links to post share buttons

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -3,4 +3,14 @@ module PostHelper
     twitter_url = "https://twitter.com/intent/tweet?url="
     twitter_url + post_url(post) + "&text=" + post.intro
   end
+
+  def facebook_share_url(post)
+    facebook_url = "http://www.facebook.com/sharer.php?u="
+    facebook_url + post_url(post) + "&p[title]=" + post.intro
+  end
+
+  def linkedin_share_url(post)
+    linkedin_url = "https://www.linkedin.com/shareArticle?mini=true&url="
+    linkedin_url + post_url(post) + "&text=" + post.intro
+  end
 end
