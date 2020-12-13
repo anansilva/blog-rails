6fc66696ea6a4d62ba6d1488d877c76fd2f278af
Ana Silva <ananunesdasilva@gmail.com>
2020-12-13 20:55:12 +0000

    Improve mobile experience

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/home/", to: "home#show"
-  root "posts#index"
+  root "home#show"
 
   match "/404", to: "errors#not_found", via: :all
   match "/422", to: "errors#unacceptable", via: :all
