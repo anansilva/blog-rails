8e787eef8a94c84f6635daf99b62b6874e043a69
Ana Silva <ananunesdasilva@gmail.com>
2020-12-27 17:03:08 +0000

    Improve meta title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-  def html_title
-    return @post.title if @post
-    return 'About me' if current_page?(about_path)
-    return 'Ana Nunes da Silva' if current_page?(home_path)
-    return 'Posts' if current_page?(posts_path)
-    return "Posts: #{params[:tag]}" if current_page?(posts_path) && params[:tag]
-
-    'Ana\'s tech blog'
-  end
 end
-
