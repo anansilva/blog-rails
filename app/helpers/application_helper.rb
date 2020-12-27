3fa3bd9d3101043c06349d138206fa63084e87f9
Ana Silva <ananunesdasilva@gmail.com>
2020-12-27 16:50:08 +0000

    fix rubocop offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
   def html_title
     return @post.title if @post
-
     return 'About me' if current_page?(about_path)
     return 'Ana Nunes da Silva' if current_page?(home_path)
     return 'Posts' if current_page?(posts_path)
