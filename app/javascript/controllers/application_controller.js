171706b3ce4b7e0087d1d0eb65e3a7a869a7a357
Ana Silva <ananunesdasilva@gmail.com>
2021-01-22 18:38:35 +0000

    Change highlight to dracula

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "stimulus"
 import hljs from 'highlight.js/lib/core'
-import 'highlight.js/styles/gruvbox-dark.css'
+import 'highlight.js/styles/atom-one-dark.css'
 
 ['javascript', 'ruby', 'erb', 'bash'].forEach((langName) => {
   const langModule = require(`highlight.js/lib/languages/${langName}`);
