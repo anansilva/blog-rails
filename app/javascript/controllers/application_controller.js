bae9c44c543832f60e9593f1da214e6bab57a1dd
Ana Silva <ananunesdasilva@gmail.com>
2021-02-20 17:39:26 +0000

    post styling adjustments (markdown and hljs css)

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -2,13 +2,13 @@ import { Controller } from "stimulus"
 import hljs from 'highlight.js/lib/core'
 import 'highlight.js/styles/atom-one-dark.css'
 
-['javascript', 'ruby', 'erb', 'bash'].forEach((langName) => {
+['javascript', 'ruby', 'erb', 'bash', 'plaintext', 'vim'].forEach((langName) => {
   const langModule = require(`highlight.js/lib/languages/${langName}`);
   hljs.registerLanguage(langName, langModule);
 });
 
 hljs.configure({
-  languages: ['ruby', 'erb', 'bash', 'javascript']
+  languages: ['ruby', 'erb', 'bash', 'javascript', 'plaintext', 'vim']
 });
 
 export default class extends Controller {
