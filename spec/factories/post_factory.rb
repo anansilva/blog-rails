cf86191bbcb60eda9ad0f308ed2c68092c7cc6e9
Ana Silva <ananunesdasilva@gmail.com>
2021-02-20 18:57:02 +0000

    fix post factory

--- spec/factories/post_factory.rb
+++ spec/factories/post_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :post do
     title { 'Sample post title' }
     intro { 'Intro' }
-    body { 'Sample post body' }
+    markdown_body { 'Sample post body' }
     cover_image {  Rack::Test::UploadedFile.new("#{::Rails.root}/spec/fixtures/abstract-done.png", 'image/png') }
   end
 end
