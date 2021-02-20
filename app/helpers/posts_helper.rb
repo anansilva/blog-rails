0ba4f2fe09d82010861b03b1fb9c5569278a1f8a
Ana Silva <ananunesdasilva@gmail.com>
2021-02-20 15:57:26 +0000

    fix rubocop offenses

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -32,7 +32,7 @@ module PostsHelper
   end
 
   def markdown_to_html(text)
-    options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks]
+    options = %i[hard_wrap autolink no_intra_emphasis fenced_code_blocks]
     Markdown.new(text, *options).to_html.html_safe
   end
 end
