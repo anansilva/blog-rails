b1d06eecfeae5d459029e5e17f52d8a31ed4da2e
Ana Silva <ananunesdasilva@gmail.com>
2021-04-11 19:29:23 +0100

    Calling the new subscriber service

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,8 +1,15 @@
 class SubscriptionsController < ApplicationController
   def new
-
+    ::Services::NewSubscriber.new(
+      subscription_params[:email],
+      referrer_url)
   end
 
+  private
+
+  def referrer_url
+    request.url
+  end
 
   def subscription_params
     params.permit(:email)
--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -2,5 +2,14 @@ require 'webmock/rspec'
 
 describe SubscriptionsController do
   describe '#new' do
+    before do
+      allow(Services::NewSubscriber).to receive(:new).and_return({})
+
+      post :new
+    end
+
+    it 'calls the NewSubscriber service' do
+      expect(Services::NewSubscriber).to have_received(:new)
+    end
   end
 end
