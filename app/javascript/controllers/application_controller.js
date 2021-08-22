817e4b0b86e4c7cdf7b1a776efedf01fd0e89f87
Ana Nunes da Silva <ananunesdasilva@MacBook-Air-4.local>
2021-08-22 16:06:19 +0100

    fix highlight.js configs

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,21 +1,21 @@
 import { Controller } from "stimulus"
 import hljs from 'highlight.js/lib/core'
-import 'highlight.js/styles/atom-one-dark.css'
+import 'highlight.js/styles/gruvbox-dark.css'
 
-['javascript', 'ruby', 'erb', 'bash', 'plaintext', 'vim'].forEach((langName) => {
+['javascript', 'ruby', 'erb', 'bash', 'shell', 'sql'].forEach((langName) => {
   const langModule = require(`highlight.js/lib/languages/${langName}`);
   hljs.registerLanguage(langName, langModule);
 });
 
 hljs.configure({
-  languages: ['ruby', 'erb', 'bash', 'javascript', 'plaintext', 'vim']
+  languages: ['ruby', 'erb', 'bash', 'javascript', 'shell', 'sql']
 });
 
 export default class extends Controller {
   static targets = []
 
   connect() {
-    document.querySelectorAll('pre').forEach((block) => {
+    document.querySelectorAll('pre code').forEach((block) => {
       hljs.highlightBlock(block)
     })
   }
